99bcd487cd0deab581fbf1337536b1f876d12333
Bimal Krishna A <bimal.libra@gmail.com>
2018-12-02 13:59:26 +0000

    Problem#1

---
+++ dailycodingproblems/Problem-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(args[0].Split(','), s => int.Parse(s));
+            int result = int.Parse(args[1]);
+            bool flag = false;
+            HashSet<int> hashSet = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (hashSet.Contains(result - arr[i])) flag = true;
+                hashSet.Add(arr[i]);
+            }
+            if (flag)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Not Present");
+            }
+        }
+    }
+}
